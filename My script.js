06e67cd1b54692b9461e131189d3378551b30135
Suppawit Wiriyapuwadol <suppawit.41218@gmail.com>
2021-08-27 15:32:17 +0700

    friday 27/08/2021

--- My script.js
+++ My script.js
@@ -1,10 +1,18 @@
-let quote = 'You shall not past';
-console.log(quote);
-console.log(typeof(quote)+' for you is '+quote);
-let boolean = true;
-console.log(boolean);
-let boo;
-boo = false;
-console.log(boo);
-console.log(typeof(boo)+' and also '+typeof(boolean));
-console.log(8/4+8/2);
+let birthYear = 2001 ;
+zodiac = birthYear%12;
+console.log(zodiac)
+switch(zodiac){
+    case 0 : console.log('monkey') ; break;
+    case 1 : console.log('rooster') ; break;
+    case 2 : console.log('dog') ; break;
+    case 3 : console.log('pig') ; break;
+    case 4 : console.log('rat') ; break;
+    case 5 : console.log('ox') ; break;
+    case 6 : console.log('tiger') ; break;
+    case 7 : console.log('rabbit') ; break;
+    case 8 : console.log('dragon') ; break;
+    case 9 : console.log('snake') ; break;
+    case 10 : console.log('horse') ; break;
+    case 11 : console.log('sheep') ; break;
+}
+
