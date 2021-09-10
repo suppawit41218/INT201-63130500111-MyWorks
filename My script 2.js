78c2e9ffe46775c18fc7fa723aa37b610862df1d
Suppawit Wiriyapuwadol <suppawit.41218@gmail.com>
2021-09-10 15:54:21 +0700

    add card game

--- My script 2.js
+++ My script 2.js
@@ -1,15 +1,29 @@
-/*let jojo=["jonathan","joseph","jotaro","josuke"]
-for(let product of jojo){
-    console.log(product.charAt(0)+product.charAt(1))
-}*/
-/*console.log(" \n The Crusaders \n");
-let jotaro={name: " Kujo Jotaro ", stand: " Star Platinum ",ability : " Power and Acuracy "}
-for(let val in jotaro){
-console.log(val+" : "+jotaro[val]);
-}*/
+class Dice {
 
-/*let rectangle ={width:5, height: 10, area:function (){return this.width*this.height}}
-console.log(rectangle)*/
-
-let daddy = Object.create({age : 45, weight : 75,height : 178})
-console.log(daddy.age);
+    constructor(gameTitle) {
+      console.log('constructor is working');
+      this._diceName = gameTitle;
+      this._faceValue = 0;
+  
+    }
+  roll() {
+  return Math.floor(Math.random() * 6) + 1;
+   }
+  
+  }
+  
+  //#1
+  let dice1 = new Dice();
+  
+  console.log(dice1);
+  console.log(dice1.roll());
+  
+  
+  
+  let dice2 = new Dice();
+  
+  
+  
+  console.log(dice2);
+  
+  console.log(dice2.roll());
--- My script 4.js
+++ My script 4.js
@@ -1,1 +1,28 @@
-let Weapons = "pol";
+
+let card = {symbol : ["Spade","Heart","Diamond","Club"]}
+card.num = null;
+
+function randomCard(){
+    let rdsym = Math.floor(Math.random()*card.symbol.length);
+    let rdnum = Math.floor(Math.random()*13);
+    return {rdsym,rdnum};
+}
+let rdcard = randomCard();
+
+function randomSymbol(){
+    return card.symbol[rdcard.rdsym];
+}
+function randomNumber(){
+    let num = rdcard.rdnum+1;
+    let state;
+    switch(num){
+        case 1 :  return state = "Ace" ;break;
+        case 11 : return state = "Jack" ;break;
+        case 12 : return state = "Queen" ;break;
+        case 13 : return state = "King" ;break;
+        default : return num ; break;
+    }
+} 
+card.symbol = randomSymbol();
+card.num = randomNumber();
+console.log(card)
